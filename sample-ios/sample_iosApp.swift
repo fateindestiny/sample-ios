8a2e3813bd281f885f3fd59e451717caf8c741db
Ethan Kim <fateindestiny.dev@gmail.com>
2024-01-17 11:01:38 +0900

    Initial Commit

---
+++ sample-ios/sample_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  sample_iosApp.swift
+//  sample-ios
+//
+//  Created by Ethan on 1/17/24.
+//
+
+import SwiftUI
+
+@main
+struct sample_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
